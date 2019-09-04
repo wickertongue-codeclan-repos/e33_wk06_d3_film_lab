b53fbb1250c3185caf44c44cda8986edf6b7cc8f
hayden <haydenwb@gmail.com>
2019-09-04 15:52:09 +0100

    all tasks completed

--- models/cinema.js
+++ models/cinema.js
@@ -5,39 +5,40 @@ const Cinema = function (films) {
 Cinema.prototype.listOfFilms = function () {
   const filmTitles = this.films.map(function (film) {
     return film.title;
-  })
+  });
   return filmTitles;
-}
+};
 
 Cinema.prototype.findFilmByTitle = function (searchedFilm) {
  return this.films.find((film) => {
     return film.title === searchedFilm;
- })
-}
+ });
+};
 
 Cinema.prototype.findFilmsByGenre = function (searchedGenre) {
   const foundFilms = this.films.filter((film) => {
     if (film.genre === searchedGenre) return film;
-  })
+  });
   return foundFilms;
-}
+};
 
 Cinema.prototype.areThereFilmsForThisYear = function (searchedYear) {
   return this.films.some((film) => {
-    return film.year === searchedYear
-  })
-}
+    return film.year === searchedYear;
+  });
+};
 
 Cinema.prototype.areAllFilmsOverThisLength = function (enteredLength) {
   return this.films.every((film) => {
-    return film.length > enteredLength
-  })
-}
+    return film.length > enteredLength;
+  });
+};
 
 Cinema.prototype.totalRunningTime = function () {
-  const totalTime = this.films.length.reduce();
-  return totalTime
-}
+  return this.films.reduce((total, film) => {
+    return total += film.length;
+  }, 0);
+};
 
 // Cinema.prototype.areThereFilmsForThisYear = function (searchedYear) {
 //   result = this.films.find(function (film) {
